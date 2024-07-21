ab95007d396c42ff6bd1376fd60e473687f3024d
kuvaldastalina <m.kopytenko@yandex.ru>
2024-07-21 07:23:23 +0300

    Add files via upload

---
+++ Test2.py
@@ -0,0 +1,7 @@
+completed = "12"
+hour = "1.5"
+course_name = "Python"
+time = (1.5 / 12)
+
+print('Курс:', course_name, ',всего задач:', completed, ',затрачено часов:', hour, ',среднее время выполнения', time, 'часа.')
+
